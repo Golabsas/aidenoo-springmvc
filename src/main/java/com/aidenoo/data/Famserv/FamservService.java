6a87605ad9d6bb6a06eb450217fcfd53a35d746c
Radoslaw Podosek <podosek.radosalw@gmail.com>
2017-05-17 11:25:27 +0200

    deplace certain fichier dans un autre package

--- src/main/java/com/aidenoo/data/Famserv/FamservService.java
+++ src/main/java/com/aidenoo/data/Famserv/FamservService.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.aidenoo.data.dao.FamservDAO;
 import com.aidenoo.data.model.Famserv;
 import com.aidenoo.security.SecurityCommon;
 
