644107b89ad3c9b8c21017c32de0bfd2153e7dcb
Rapod <podosek.radoslaw@gmail.com>
2017-05-24 12:33:39 +0200

    Ajout d'une detection si le login entrer est correcte sinon annule l'authentification en la remplacant par un identifiant inexistant.

--- src/main/java/com/aidenoo/security/LoggedUserDetail.java
+++ src/main/java/com/aidenoo/security/LoggedUserDetail.java
@@ -11,34 +11,38 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import com.aidenoo.data.Login.Login;
 import com.aidenoo.data.Login.LoginDAO;
 
 @Service("userDetailsService")
 public class LoggedUserDetail implements UserDetailsService {
-	
+
 	@Autowired
 	LoginDAO db;
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		
+
 		LoggedUser aidenooUser = null;
 		List<GrantedAuthority> authorities = new ArrayList<>();
-		
-		if(!"".equals(username)) {
+
+		if (!"".equals(username)) {
 			
-			// TODO: Verifier que le login entrer est dans la BDD
-			
-			authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
-			
-			aidenooUser = new LoggedUser(username, 
-					db.readPassword(username), true, true, true, true, authorities, 
-					username, db.readSociete(username));
-			
+			try {
+				Login user = db.search(username);
+
+				authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRole()));
+
+				aidenooUser = new LoggedUser(username, user.getPasswordx(), true, true, true, true, authorities,
+						username, user.getIdsociete());
+			} catch (Exception e) {
+				aidenooUser = new LoggedUser("NA", "NA", true, true, true, true, authorities, null, null);
+			}
+
 		} else {
 			aidenooUser = new LoggedUser("NA", "NA", true, true, true, true, authorities, null, null);
 		}
-		
+
 		return aidenooUser;
 	}
 }
