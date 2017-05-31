3982cddf1465e5ff1fbdcf275af16ea1bd60079a
Rapod <podosek.radoslaw@gmail.com>
2017-05-31 18:40:33 +0200

    Mise a jour de l'authemtification pour correspondre au best practice de la doc officiel

--- src/main/java/com/aidenoo/security/SecurityConfiguration.java
+++ src/main/java/com/aidenoo/security/SecurityConfiguration.java
@@ -1,15 +1,21 @@
 package com.aidenoo.security;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import com.aidenoo.data.Login.Login;
 import com.aidenoo.data.Login.LoginDAO;
@@ -30,23 +36,26 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
     }
 
-	@Autowired
-	public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception {
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
 		
 		List<Login> logins = db.read();
 		
-		for (Login login : logins) {
-			auth.inMemoryAuthentication()
-				.withUser(login.getLogin())
-				.password(login.getPasswordx())
-				.roles("USER");
-		}
+		for (Login login : logins) 
+			manager.createUser( LoggedUser
+					.withUsername(login.getLogin())
+					.password(login.getPasswordx())
+					.roles(login.getRole()).build());
+		
+		return manager;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().antMatchers("/login").permitAll()
-				.antMatchers("/", "/*todo*/**", "/**").access("hasRole('USER')").and()
+		http.authorizeRequests().antMatchers("/login","/logout").permitAll()
+				.antMatchers("/", "/*todo*/**", "/*list*/**").access("hasRole('USER')").and()
 				.formLogin();
 	}
 }
